33509841174c4673e0667c5bd096bc2d6242f474
LineCatOvO <linecatpersonal@outlook.com>
2025-09-10 13:53:43 +0800

    changed to snake_case

--- scene/player/settings_manager.cs
+++ scene/player/settings_manager.cs
@@ -1,4 +1,6 @@
-﻿using Godot;
+﻿// SHOULD BE ADDED TO AUTOLOAD IN GODOT
+
+using Godot;
 using TaikoLine.entity;
 
 namespace TaikoLine;
@@ -9,6 +11,8 @@ public partial class SettingsManager : Node
     public override void _Ready()
     {
         LoadSettings();
+        // Engine.MaxFps = _settings.MaxFps;
+        Engine.MaxFps = 60;
     }
 
     private void LoadSettings()
--- scene/player/track_background.cs
+++ scene/player/track_background.cs
@@ -6,6 +6,5 @@ public partial class TrackBackground : Sprite2D
     public override void _Draw()
     {
         Rect2 window = GetViewportRect();
-        DrawRect(new Rect2(new Vector2(0f,window.Size.y*ContentManager), GetViewportRect().Size));
     }
 }
